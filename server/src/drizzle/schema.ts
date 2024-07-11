94f094dc28f30fc4e4a8c9f1ed76b54ed79344cf
Oscar  Diaz <oscardiaz@Oscars-MacBook-Pro.local>
2024-07-10 21:18:44 -0600

    adding user team and positions table

--- server/src/drizzle/schema.ts
+++ server/src/drizzle/schema.ts
@@ -1,17 +1,95 @@
+import { relations } from "drizzle-orm";
 import {
   serial,
   timestamp,
   pgTable,
   varchar,
+  integer,
   boolean,
+  primaryKey,
 } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: serial("id").primaryKey().notNull(),
-  email: varchar("email").notNull(),
+  email: varchar("email").notNull().default(""),
   username: varchar("username"),
-  password: varchar("password").notNull(),
+  password: varchar("password").notNull().default(""),
   isConfirmed: boolean("isConfirmed").default(false),
+  firstName: varchar("firstName").notNull().default(""),
+  lastName: varchar("lastName").notNull().default(""),
+  dateOfBirth: varchar("dateOfBirth").notNull().default(""),
+  country: varchar("country").notNull().default(""),
+  createdAt: timestamp("createdAt"),
+  updatedAt: timestamp("updatedAt"),
+  teamId: integer("teamId"),
+});
+
+// relations for the users table
+export const usersRelations = relations(usersTable, ({ one, many }) => ({
+  team: one(teamsTable, {
+    fields: [usersTable.teamId],
+    references: [teamsTable.id],
+  }),
+  positions: many(positionsTable),
+}));
+
+export const teamsTable = pgTable("teams", {
+  id: serial("id").primaryKey().notNull(),
+  name: varchar("name").notNull().default(""),
+  country: varchar("country").notNull().default(""),
+  logoUrl: varchar("logoUrl").notNull().default(""),
   createdAt: timestamp("createdAt"),
   updatedAt: timestamp("updatedAt"),
 });
+
+// relations for the teams table
+export const teamRelations = relations(teamsTable, ({ one, many }) => ({
+  users: many(usersTable),
+}));
+
+export const positionsTable = pgTable("positions", {
+  id: serial("id").primaryKey().notNull(),
+  abbreviation: varchar("abbreviation").notNull().default(""),
+  longName: varchar("longName").notNull().default(""),
+  fieldIdentifier: integer("fieldIdentifier").notNull().default(0),
+  createdAt: timestamp("createdAt"),
+  updatedAt: timestamp("updatedAt"),
+});
+
+// relations for the positions table
+export const positionRelations = relations(positionsTable, ({ one, many }) => ({
+  users: many(usersTable),
+}));
+
+// junction table for many-to-many relationship between users and positions
+export const userPositionsTable = pgTable(
+  "user_positions",
+  {
+    userId: integer("userId")
+      .notNull()
+      .references(() => usersTable.id),
+    positionId: integer("positionId")
+      .notNull()
+      .references(() => positionsTable.id),
+  },
+  (junctionTable) => ({
+    pk: primaryKey({
+      columns: [junctionTable.userId, junctionTable.positionId],
+    }),
+  })
+);
+
+// relations for users positions table
+export const userPositionsRelations = relations(
+  userPositionsTable,
+  ({ one }) => ({
+    user: one(usersTable, {
+      fields: [userPositionsTable.userId],
+      references: [usersTable.id],
+    }),
+    position: one(positionsTable, {
+      fields: [userPositionsTable.positionId],
+      references: [positionsTable.id],
+    }),
+  })
+);
